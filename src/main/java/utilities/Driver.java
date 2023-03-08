2b70f8687e8724f21b5789f2296e7cf92df76373
Mehmet Buyukertas <73278559+Mehmet6327@users.noreply.github.com>
2023-03-08 14:21:57 +0100

    Update Driver.java

--- src/main/java/utilities/Driver.java
+++ src/main/java/utilities/Driver.java
@@ -1,2 +1,49 @@
-package utilities;public class Driver {
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+
+public class Driver {
+    private static WebDriver driver;
+    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+
+
+    private Driver() {
+    }
+
+    public static WebDriver getDriver() {
+        //create the driver if and only if it is null
+        if (driver == null) {
+            String browser = ConfigurationReader.getProperty("browser");
+            if ("chrome".equals(browser)) {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            } else if ("firefox".equals(browser)) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            } else if ("ie".equals(browser)) {
+                WebDriverManager.iedriver().setup();
+                driver = new InternetExplorerDriver();
+            } else if ("safari".equals(browser)) {
+                WebDriverManager.getInstance(SafariDriver.class).setup();
+                driver = new SafariDriver();
+            } else if ("chrome-headless".equals(browser)) {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
+            }
+        }
+        driver.manage().window().maximize();
+        return driver;
+
+    }
+    public static void closeDriver() {
+        driverPool.get().quit();
+        driverPool.remove();
+    }
 }
