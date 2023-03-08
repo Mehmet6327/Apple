5de9aceebb8559aa74ebe58037b677397f5924cc
Mehmet Buyukertas <73278559+Mehmet6327@users.noreply.github.com>
2023-03-08 14:20:37 +0100

    Update CukesRunner.java

--- src/main/java/runners/CukesRunner.java
+++ src/main/java/runners/CukesRunner.java
@@ -1,2 +1,18 @@
-package runners;public class CukesRunner {
+package runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/main/resources/features",
+        plugin = {"html:target/html-reports/cucumber.html"},
+        glue = "stepDefinitions",
+        dryRun =false,
+        tags = "@wip"
+)
+public class CukesRunner {
+
 }
