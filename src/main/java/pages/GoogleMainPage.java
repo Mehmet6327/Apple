424674b7803e41c54b1a889ba8e3b988f5bb6c51
Mehmet Buyukertas <73278559+Mehmet6327@users.noreply.github.com>
2023-03-08 14:20:09 +0100

    Update GoogleMainPage.java

--- src/main/java/pages/GoogleMainPage.java
+++ src/main/java/pages/GoogleMainPage.java
@@ -1,2 +1,27 @@
-package pages;public class GoogleMainPage {
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class GoogleMainPage {
+    public GoogleMainPage (){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+    @FindBy(xpath ="(//button[@class='tHlp8d']//div)[2]")
+    public WebElement aceptarTodo;
+
+    @FindBy(name = "q")
+    public WebElement searchBox;
+
+    @FindBy(id = "result-stats")
+    public WebElement resultBox;
+
+
+
+
+
+
+
 }
